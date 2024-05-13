37739ebea50856248b8b4eb0f0bd9d7e8535c2c4
fayadin <fadiyahadiningsih90@gmail/com>
2024-05-13 17:40:58 +0900

    fix shits

--- api.ts
+++ api.ts
@@ -1,6 +1,6 @@
 import { ITask } from "./types/tasks";
 
-const baseUrl = 'http://localhost:3001'
+const baseUrl = `${process.env.NEXT_PUBLIC_API_LINK}`
 
 export const getAllTodos = async (): Promise<ITask[]> => {
     const res = await fetch(`${baseUrl}/tasks`, {cache: 'no-store'});
--- data/pages/api/todo.js
+++
@@ -1,5 +0,0 @@
-import data from '../../todos.json';
-
-export default function handler(req, res) {
-  res.status(200).json(todos);
-}
